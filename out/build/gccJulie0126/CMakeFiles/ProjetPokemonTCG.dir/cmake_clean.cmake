3fa0ec0483eaaee13e1fa1cddae5b7779774fc69
Julie Aubry <julie.aubry26@gmail.com>
2026-01-20 00:20:07 +0100

    MARCHE PAS nouvelle architecture propre

---
+++ out/build/gccJulie0126/CMakeFiles/ProjetPokemonTCG.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetPokemonTCG.dir/jeu.cpp.o"
+  "CMakeFiles/ProjetPokemonTCG.dir/jeu.cpp.o.d"
+  "ProjetPokemonTCG"
+  "ProjetPokemonTCG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetPokemonTCG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
