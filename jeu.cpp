6f36ea7369fd14105d08841d7e2093f19a5c2c2e
Julie Aubry <julie.aubry26@gmail.com>
2026-01-20 01:17:30 +0100

    je crois que je néttoie main ???

--- jeu.cpp
+++
@@ -1,6 +0,0 @@
-#include "carte.hpp"
-#include "joueur.hpp"
-
-int main() {
-    auto pokemons = loadPokemon("cards_debut.json") ;
-}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "carte.hpp"
+#include "joueur.hpp"
+
+int main() {
+
+    auto pokemons = loadPokemon("cards_debut.json") ;
+}
