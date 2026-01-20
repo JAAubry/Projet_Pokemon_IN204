07a69381b5b4458f7928c62db2d8075d717566bc
Julie Aubry <julie.aubry26@gmail.com>
2026-01-20 20:15:43 +0100

    Modification du Cmake pour prendre en compte les graphismes SFML 2.6.1

---
+++ build/CMakeFiles/ProjetPokemonTCG.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetPokemonTCG.dir/src/main.cpp.o"
+  "CMakeFiles/ProjetPokemonTCG.dir/src/main.cpp.o.d"
+  "ProjetPokemonTCG"
+  "ProjetPokemonTCG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetPokemonTCG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
